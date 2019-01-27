b999594355bd6eb40113fc60a6dff1ac0cd55e64
四熊泰之 <shikuma@me.com>
2019-01-27 12:49:47 +0900

    Initial Commit

---
+++ DataManager/DataManager.h
@@ -0,0 +1,19 @@
+//
+//  DataManager.h
+//  DataManager
+//
+//  Created by 四熊泰之 on 2019/01/27.
+//  Copyright © 2019 四熊泰之. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DataManager.
+FOUNDATION_EXPORT double DataManagerVersionNumber;
+
+//! Project version string for DataManager.
+FOUNDATION_EXPORT const unsigned char DataManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataManager/PublicHeader.h>
+
+
