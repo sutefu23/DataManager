8bb70fcf57291eb0102c72cf7ea3fec7ae701676
shikuma <shikuma@mac.com>
2019-08-19 00:08:28 +0900

    バージョン1.00とする 更新履歴を追加する 新バージョンがある場合、新バージョンまでの更新履歴を表示する

---
+++ DataManager/Utility/Month.swift
@@ -0,0 +1,23 @@
+//
+//  Month.swift
+//  DataManager
+//
+//  Created by manager on 8/17/1 R.
+//  Copyright © 1 Reiwa 四熊泰之. All rights reserved.
+//
+
+import Foundation
+
+public struct Month {
+    public let year : Int
+    public let month : Int
+    
+    public init(year:Int, month:Int) {
+        self.init(year, month)
+    }
+    init(_ year:Int, _ month:Int) {
+        self.year = year
+        self.month = month
+    }
+
+}
