28fa0f394edbb4e0326534b9645cdde937ec98fb
shikuma <las@osakaname.co.jp>
2019-12-03 11:57:48 +0900

    ファイバー対応 円弧描画バグ修正

--- DataManager/pm_osakaname/指示書文字数.swift
+++ DataManager/pm_osakaname/指示書文字数.swift
@@ -11,6 +11,7 @@ import Foundation
 private let serialQueue: OperationQueue = {
    let queue = OperationQueue()
     queue.maxConcurrentOperationCount = 1
+    queue.qualityOfService = .utility
     return queue
 }()
 
