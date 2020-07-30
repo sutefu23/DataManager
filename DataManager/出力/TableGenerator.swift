d65eeb0b59cfc6baf70fb06ccc880469a692a46b
sutefu23 <sutefu23@gmail.com>
2020-07-30 15:57:42 +0900

    TimeIntervalで０の時は出さないように

--- DataManager/出力/TableGenerator.swift
+++ DataManager/出力/TableGenerator.swift
@@ -300,8 +300,8 @@ public extension TableGenerator {
             case .minute0:
                 //小数点第一位以下は切り捨て（旧CSVエクスポーターにデータを合わせるため
                 let value = Int(value/60)
-                //結果が０より下の時はnilを返す
-                if value < 0 { return nil }
+                //結果が０以下の時はnilを返す
+                if value <= 0 { return nil }
                 return String(value)
             case .minute1:
                 return String(format: "%.1f", value/60)
