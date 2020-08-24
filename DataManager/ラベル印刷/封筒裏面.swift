e574d3b7aa64288196ed4e472fbea248e25cf7a0
shikuma <las@osakaname.co.jp>
2020-08-24 13:46:26 +0900

    封筒裏面の一部文字差しかえ PDF追加（基本使わない）

--- DataManager/ラベル印刷/封筒裏面.swift
+++ DataManager/ラベル印刷/封筒裏面.swift
@@ -9,7 +9,20 @@
 import Foundation
 import PDFKit
 
+public func make封筒裏面2(_ count: Int) -> PDFDocument {
+    guard let url = Bundle(for: PaperPDFPage.self).url(forResource: "取付付属品表裏面", withExtension: "pdf"),
+          let pdf = PDFDocument(url: url), pdf.pageCount > 0
+    else { return PDFDocument() }
+    for _ in 1..<count {
+        let page = pdf.page(at: 0)!
+        page.copy()
+        pdf.insert(page, at: pdf.pageCount-1)
+    }
+    return pdf
+}
+
 public func make封筒裏面(_ count: Int) -> PDFDocument {
+    if count < 0 { return make封筒裏面2(-count) }
     let count = max(1, count)
     assert(count >= 1)
     let lx = 24.0
@@ -31,25 +44,25 @@ public func make封筒裏面(_ count: Int) -> PDFDocument {
         let rect = PaperRect(px: 10, py: 10, pwidth: 646, pheight: 320)
         let text1 = PaperText(mmx: lx+bigdx, mmy: y1, text: "【注意事項】", fontSize: bigFont, bold: false, color: .black, nooffset: true)
         let text1b = PaperText(mmx: lx, mmy: y1+bigHeight, text: """
-■研磨材・タワシ・ペーパー等、表面に傷を付ける恐れのあるものは、絶対に使用しないでください。
-■溶解性(アルカリ性・酸性の洗剤など)・揮発性(シンナー・アルコールなど)の薬品は絶対に使用しないで下さい。
-■塗装面(真鍮・銅・メッキ製品への透明クリアも含む)に、テープ類は貼らないで下さい。
+\u{25FC}研磨材・タワシ・ペーパー等、表面に傷を付ける恐れのあるものは、絶対に使用しないでください。
+\u{25FC}溶解性(アルカリ性・酸性の洗剤など)・揮発性(シンナー・アルコールなど)の薬品は絶対に使用しないで下さい。
+\u{25FC}塗装面(真鍮・銅・メッキ製品への透明クリアも含む)に、テープ類は貼らないで下さい。
 """, fontSize: normalFont, bold: false, color: .black, nooffset: true, lineSpace: lineSpace)
         
         let text2 = PaperText(mmx: lx+bigdx, mmy: y2, text: "【お手入れ方法】", fontSize: bigFont, bold: false, color: .black, nooffset: true)
         let text2b = PaperText(mmx: lx, mmy: y2+bigHeight, text: """
-■まずは、製品に付着したチリや埃をやわらかい布で落とした後、水を含ませた布で、
+\u{25FC}まずは、製品に付着したチリや埃をやわらかい布で落とした後、水を含ませた布で、
 　やさしくなでる様に表面の目に沿って拭いて下さい。
 　その後は、必ず柔らかい布などを使用し、同じ要領で乾拭きを行って下さい。
-■浮かして取り付けてある商品は、強い力を加えないように作業を行ってください。
-■製品によっては鋭利な部分があるので、お手入れの際は十分にお気をつけください。
+\u{25FC}浮かして取り付けてある商品は、強い力を加えないように作業を行ってください。
+\u{25FC}製品によっては鋭利な部分があるので、お手入れの際は十分にお気をつけください。
 """, fontSize: normalFont, bold: false, color: .black, nooffset: true, lineSpace: lineSpace)
         
         let text3 = PaperText(mmx: lx+bigdx, mmy: y3, text: "【錆や変色の主な原因】", fontSize: bigFont, bold: false, color: .black, nooffset: true)
         let text3b = PaperText(mmx: lx, mmy: y3+bigHeight, text: """
-■ステンレス製品は錆びにくい素材であり、決して錆びない素材ではありません。
+\u{25FC}ステンレス製品は錆びにくい素材であり、決して錆びない素材ではありません。
 　工場現場や線路のそばなど、飛んできた鉄粉や粉塵の付着によって、もらい錆を受けます。
-■銅・真鍮製品は必ず変色する素材です。弊社では変色を遅らせる為に透明クリア塗装を施しております。
+\u{25FC}銅・真鍮製品は必ず変色する素材です。弊社では変色を遅らせる為に透明クリア塗装を施しております。
 　しかし、特に屋外で使用される場合においては、短期間で変色する場合があります。
 """, fontSize: normalFont, bold: false, color: .black, nooffset: true, lineSpace: lineSpace)
         
