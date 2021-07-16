da353dbc162e78b7fec2df47b8c8933f6715a75b
sutefu23 <sutefu23@gmail.com>
2021-07-16 17:07:16 +0900

    出荷実績を添付ファイルにて自動メール送信

--- Sources/DataManager/Resources/sendMail.py
+++ Sources/DataManager/Resources/sendMail.py
@@ -1,17 +1,21 @@
 # coding: UTF-8
 
 import sys
+import pathlib
 import base64
 import smtplib
 import ssl
+import os
 from email.mime.text import MIMEText
 from email.utils import formatdate
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 
-import mailConfig as config
+import json
 
 try:
     if len(sys.argv) < 5:
-        raise AttributeError("コマンドライン引数は最低5つ必要です（送信元メール、受信先メール、CC、タイトル、本文）")
+        raise AttributeError("コマンドライン引数は最低5つ必要です（送信元メール、受信先メール、CC、タイトル、本文、(オプション)添付ファイル）")
 
 
     mail_from = sys.argv[1]
@@ -19,22 +23,33 @@ try:
     mail_to = sys.argv[2]
 
     mail_cc = sys.argv[3]
-    
-    title = sys.argv[4]
 
-    body = " ".join(sys.argv[5:]) #本文中に半角スペースが入ってる可能性を勘案
+    attach = sys.argv[4]
+
+    title = sys.argv[5]
+
+    body = " ".join(sys.argv[6:]) #本文中に半角スペースが入ってる可能性を勘案
+
+    msg = MIMEMultipart()
 
     charset = "utf-8"
     if charset == "utf-8":
-        msg = MIMEText(body, "plain", charset)
+        bd = MIMEText(body, "plain", charset)
     elif charset == "iso-2022-jp":
-        msg = MIMEText(base64.b64encode(body.encode(charset, "ignore")), "plain", charset)
+        bd = MIMEText(base64.b64encode(body.encode(charset, "ignore")), "plain", charset)
+    msg.attach(bd)
+
+    config_file = os.path.join(pathlib.Path(__file__).parent.absolute() , "mailConfig.json")
+    # 保存したファイルを読み込む
+    with open(config_file, "r") as f:
+        conf = json.load(f)
+    
 
 
     """
     (2) MIMEメッセージに必要なヘッダを付ける
     """
-    msg.replace_header("Content-Transfer-Encoding", "base64")
+    msg.add_header("Content-Transfer-Encoding", "base64")
     msg["Subject"] = title
     msg["From"] = mail_from
     msg["To"] = mail_to
@@ -42,15 +57,21 @@ try:
     msg["Bcc"] = ""
     msg["Date"] = formatdate(None,True)
 
+    # 添付ファイル
+    if len(attach) != 0:
+        with open(attach, "rb") as a:
+            mb = MIMEApplication(a.read(), Name="出荷実績.txt")
+        mb.add_header("Content-Disposition", "attachment", filename="出荷実績.txt")
+        msg.attach(mb)
 
     """
     (3) SMTPクライアントインスタンスを作成
     """
-    host = config.host
-    port = config.port
-    method = config.method
+    host = conf["host"]
+    port = conf["port"]
+    method = conf["method"]
 
-    port = config.port
+    port = conf["port"]
     if method == "noencrypt":
         smtpclient = smtplib.SMTP(host, port, timeout=10)
     elif method == "starttls":
@@ -67,8 +88,8 @@ try:
     """
     (4) サーバーにログインする
     """
-    username = config.username
-    password = config.password
+    username = conf["username"]
+    password = conf["password"]
     smtpclient.login(username, password)
 
 
@@ -77,6 +98,6 @@ try:
     """
     smtpclient.send_message(msg)
     smtpclient.quit()
-except:
+except Exception as e:
     # エラーを返す(先頭の!がエラーの印)
-    print('!SendMailError', end='')
+    print('!SendMailError:',str(sys.exc_info()[0]), str(sys.exc_info()[1]), end='')
--- Sources/DataManager/Utility/SendMail.swift
+++ Sources/DataManager/Utility/SendMail.swift
@@ -7,7 +7,47 @@
 
 import Foundation
 
-enum MailError: LocalizedError {
+
+public struct MailServer: Codable {
+    var host: String
+    var username: String
+    var password: String
+    var port: Int
+    var method: MailMethod
+    public init(host: String , username: String, password: String, port: Int, method: MailMethod){
+        self.host = host
+        self.username = username
+        self.password = password
+        self.port = port
+        self.method = method
+    }
+}
+
+public enum MailMethod: String, Codable{
+    public typealias RawValue = String
+    case noEncript = "rawvalue"
+    case startTLS = "starttls"
+    case SSL = "ssl"
+}
+
+public struct MailBody {
+    var from: String
+    var to: String
+    var cc: String = ""
+    var title: String
+    var body: String
+    var file: URL?
+    public init(from: String, to: String, cc: String, title: String, body:String, file: URL? = nil){
+        self.from = from
+        self.to = to
+        self.cc = cc
+        self.title = title
+        self.body = body
+        self.file = file
+    }
+}
+
+public enum MailError: LocalizedError {
     case notSupport
     case spaceNotAllowInSpace
     case notValidMailFormat
@@ -28,7 +68,7 @@ enum MailError: LocalizedError {
 }
 
 @available(OSX 10.13, *)
-public func sendMail(mail_from: String ,mail_to: String, mail_cc: String , title: String, body: String) throws {
+public func sendMail(mail: MailBody, server: MailServer) throws {
     #if os(macOS) || os(Linux)
     let process = Process()
     process.executableURL = URL(fileURLWithPath: "/usr/bin/python3")
@@ -39,10 +79,35 @@ public func sendMail(mail_from: String ,mail_to: String, mail_cc: String , title
         return
     }
 
-    
+    ///サーバー設定のjson保存(sendMail.pyから参照)
+    let jsonEncoder = JSONEncoder()
+    do {
+        let jsonData = try jsonEncoder.encode(server)
+        guard let json = String(data: jsonData, encoding: .utf8) else {
+            return
+        }
+        guard let confFile = bundle.url(forResource: "mailConfig", withExtension: "json") else {
+            NSLog("mailConfig.jsonが見つかりません")
+            return
+        }
+        
+        try json.write(to: confFile, atomically: false, encoding: .utf8)
 
-    //コマンドライン引数＝[送信元、受信先、タイトル、本文]
-    process.arguments = [app.path, mail_from, mail_to, mail_cc, title, body]
+    } catch let error {
+        error.showAlert()
+    }
+
+    /// コマンドライン引数＝[送信元、受信先、タイトル、本文、（添付ファイル）]
+    var attach = ""
+    if let file = mail.file {
+        /// 一時ファイルとしてコピー
+        let tmp = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(file.filename + "." + file.pathExtension )
+        try? FileManager.default.removeItem(at: tmp)
+        try FileManager.default.copyItem(at: file, to: tmp)
+        attach = tmp.path
+    }
+
+    process.arguments = [app.path, mail.from, mail.to, mail.cc, attach, mail.title, mail.body ]
 
     let pipe = Pipe()
     process.standardOutput = pipe
@@ -53,8 +118,7 @@ public func sendMail(mail_from: String ,mail_to: String, mail_cc: String , title
     guard let returnValue = String(data: data, encoding: .utf8) else{ return }
     
     if returnValue.hasPrefix("!") {
-
-        
+        showMessage(message: "メール送信に失敗しました。\n" + returnValue)
     }
 
     #else
@@ -62,7 +126,7 @@ public func sendMail(mail_from: String ,mail_to: String, mail_cc: String , title
     #endif
     
 }
-func isValidEmail(_ string: String) -> Bool {
+public func isValidEmail(_ string: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         let result = emailTest.evaluate(with: string)
